b6f90f98dda876abd65297547ec79695d12b11b8
mbb00468 <124305818+mbb00468@users.noreply.github.com>
2023-11-27 23:14:22 -0500

    chat

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from transformers import ViTImageProcessor, ViTForImageClassification
+from flask import Flask, render_template, request, flash, redirect
 from PIL import Image
-from flask import Flask, render_template, request
-import os
-from flask import flash, redirect
-from ultralytics import YOLO
-from PIL import Image
-import warnings
-import io
 from io import BytesIO
 import base64
+import os
+from ultralytics import YOLO
+import warnings
 
 warnings.filterwarnings('ignore')
 
@@ -18,20 +15,21 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = "super_secret_key"  # for flashing messages
+app.secret_key = os.urandom(24)  # Use a random secret key for security
 
 # Ensure the upload directory exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def predict_vit(image):
-    # Load the ViT model and processor
-    feature_extractor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
-
     try:
+        # Load the ViT model and processor outside the function
+        feature_extractor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+        model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
         # Extract features (patches) from the image
         inputs = feature_extractor(images=image, return_tensors="pt")
 
@@ -49,88 +47,71 @@ def predict_vit(image):
     except Exception as e:
         return f"Error predicting with ViT: {str(e)}"
 
-    
-
-
-
-
-
 def predict_yolo(image):
-    yolo_model = YOLO('yolov8n.pt')
-    
     try:
+        yolo_model = YOLO('yolov8n.pt')
         results = yolo_model(image, verbose=False)  # results list
 
-        for r in results:
-            im_array = r.plot()  # plot a BGR numpy array of predictions
-            im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-            # im.show()  # show image
-            im.save('static/results.png')  # save image
+        pred_classes = []
 
-        predclass = []
-
-        for result in results:  # iterate results
+        for result in results:
             boxes = result.boxes.cpu().numpy()  # get boxes on the CPU in numpy
             for box in boxes:  # iterate boxes
-                predclass.append(result.names[int(box.cls[0])])
+                pred_classes.append(result.names[int(box.cls[0])])
 
-        return predclass  # return the list of predicted classes
-
+        return pred_classes  # return the list of predicted classes
     except Exception as e:
         return f"Error predicting with YOLO: {str(e)}"
-
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return render_template('home.html', error='No file part')
+            flash('No file part')
+            return redirect(request.url)
 
         file = request.files['file']
 
-        return render_template('model_choice.html', filename=file.filename)
-    else:
-        # Render the HTML form for GET requests
-        return render_template('home.html')
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
 
+        if file and allowed_file(file.filename):
+            return render_template('model_choice.html', filename=file.filename)
+        else:
+            flash('Invalid file type. Allowed types are png, jpg, jpeg, gif.')
+            return redirect(request.url)
+    else:
+        return render_template('home.html')
 
 @app.route('/model_choice', methods=['POST'])
 def model_choice():
-    model_choice = request.form.get('model_choice')
-    file = request.files['file']
-
-    if model_choice == 'vit':
-        image = Image.open(file)
-        result = predict_vit(image)
-        return render_template('vit_result.html', result=result)
-
-    elif model_choice == 'yolo':
-        result = predict_yolo(file)
-        return render_template('yolo_result.html', result=result)
-
-    else:
-        flash('Invalid model choice')
+    if 'file' not in request.files:
+        flash('No file part')
         return redirect(request.url)
 
-    
-@app.route('/yolo_result')
-def yolo_result():
-    try:
-        file = request.files['file']
-        pred_classes = predict_yolo(file)
+    file = request.files['file']
 
-        image_bytes = BytesIO()
-        image = Image.open(file)
-        image.save(image_bytes, format='PNG')
-        image_base64 = 'data:image/png;base64,' + base64.b64encode(image_bytes.getvalue()).decode('utf-8')
+    model_choice = request.form.get('model_choice')
 
-        return render_template('yolo_result.html', pred_classes=pred_classes, result_image=image_base64)
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
 
-    except Exception as e:
-        return f"Error: {str(e)}"
+    if file and allowed_file(file.filename):
+        if model_choice == 'vit':
+            result = predict_vit(file)
+            return render_template('vit_result.html', result=result)
 
-
+        elif model_choice == 'yolo':
+            result = predict_yolo(file)
+            return render_template('yolo_result.html', result=result)
+        else:
+            flash('Invalid model choice')
+            return redirect(request.url)
+    else:
+        flash('Invalid file type. Allowed types are png, jpg, jpeg, gif.')
+        return redirect(request.url)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
