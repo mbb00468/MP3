10f1cbfdf62fb4dd0a0b32be30e10bf3ae9f090b
mbb00468 <124305818+mbb00468@users.noreply.github.com>
2023-11-28 14:12:50 -0500

    dd

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from transformers import ViTImageProcessor, ViTForImageClassification
-from flask import Flask, render_template, request, flash, redirect, url_for
-from PIL import Image
-from io import BytesIO
-import base64
-import os
-from ultralytics import YOLO
-import warnings
+from flask import Flask, request, session, redirect, url_for, render_template
 
-warnings.filterwarnings('ignore')
+import os
+from PIL import Image
+from torchvision import transforms
+from transformers import ViTImageProcessor, ViTForImageClassification
+from werkzeug.utils import secure_filename
+import torch
+from torchvision import transforms
+from PIL import Image
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+import numpy as np
+from ultralytics import YOLO
+
 
 app = Flask(__name__)
 
@@ -15,7 +19,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = os.urandom(24)  # Use a random secret key for security
+app.secret_key = "super_secret_key"  # for flashing messages
 
 # Ensure the upload directory exists
 if not os.path.exists(UPLOAD_FOLDER):
@@ -24,23 +28,80 @@ if not os.path.exists(UPLOAD_FOLDER):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# Import statements (add the necessary imports)
 
-def predict_vit(image):
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    # If the user does not select a file, the browser also submits an empty part without a filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+
+    # Check if the file has an allowed extension
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)  # Ensure a safe filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        session['file_path'] = file_path  # Save the full file path in the session
+        print('File successfully uploaded and saved!')
+        return render_template('model_choice.html')
+
+    flash('Invalid file type. Please upload an image.')
+    return redirect(request.url)
+
+
+@app.route('/model_choice', methods=['GET', 'POST'])
+def model_choice():
+    try:
+        if request.method == 'POST':
+            model_choice = request.form.get('model_choice')
+
+            # Get the file path from the session or form data
+            file_path = session.get('file_path')
+
+            if not file_path:
+                flash('File path not found. Please upload a file.')
+                return redirect(url_for('index'))
+
+            if model_choice == 'vit':
+                result = predict_vit(file_path)
+                return render_template('vit_result.html', result=result)
+
+            elif model_choice == 'yolo':
+                result_image_path, bounding_boxes = predict_yolo(file_path)
+                return render_template('yolo_result.html', result_image=result_image_path,
+                                       bounding_boxes=bounding_boxes)
+
+    except KeyError:
+        flash('File path not found in the session. Please upload a file.')
+        return redirect(url_for('index'))
+    except Exception as e:
+        flash(f"An error occurred: {str(e)}")
+        print(f"An error occurred: {str(e)}")
+        print('An error occurred. Please try again.')
+        return render_template('model_choice.html')
+
+
+def predict_vit(file_path):
     try:
         # Load the ViT model and processor outside the function
-        feature_extractor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+        feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
         model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
 
         # Open the image and normalize pixel values
-        image = Image.open(image)
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-        ])
-        image = transform(image)
-
-        # Extract features (patches) from the image
-        inputs = feature_extractor(images=image.unsqueeze(0))  # Add a batch dimension
+        image = Image.open(file_path)
+        inputs = feature_extractor(images=image, return_tensors="pt")
 
         # Predict by feeding the model
         outputs = model(**inputs)
@@ -49,107 +110,51 @@ def predict_vit(image):
         logits = outputs.logits
 
         # Model predicts one of the classes by picking the logit with the highest probability
-        predicted_class_idx = logits.argmax(-1).item()
+        predicted_class_idx = torch.argmax(logits, dim=1).item()
 
+        # In this example, we assume model.config.id2label is available
         predicted_class = model.config.id2label[predicted_class_idx]
         return predicted_class
     except Exception as e:
         return f"Error predicting with ViT: {str(e)}"
 
-def predict_yolo(image):
+
+def predict_yolo(file_path):
     try:
-        yolo_model = YOLO('yolov8n.pt')
-        results = yolo_model(image, verbose=False)  # results list
+        # Define the predict_yolo function
+        model = YOLO('yolov8n.pt')
+        results = model(file_path)
 
-        pred_classes = []
-
+        # Extract bounding boxes and labels
+        bounding_boxes = []
         for result in results:
-            boxes = result.boxes.cpu().numpy()  # get boxes on the CPU in numpy
-            for box in boxes:  # iterate boxes
-                pred_classes.append(result.names[int(box.cls[0])])
+            for box in result.boxes.xyxy:
+                # Ensure that there are at least 6 elements in the box array before accessing index 5
+                if len(box) >= 6:
+                    bounding_boxes.append({
+                        'left': box[0].item(),
+                        'top': box[1].item(),
+                        'right': box[2].item(),
+                        'bottom': box[3].item(),
+                        'label': model.names[int(box[5].item())],
+                    })
 
-        return pred_classes  # return the list of predicted classes
+        # Display and save the results
+        for i, result in enumerate(results):
+            im_array = result.plot()
+            im = Image.fromarray(im_array[..., ::-1])
+            result_image_path = f'result_{i}.jpg'
+            im.save(result_image_path)
+            print(result_image_path)
+
+        print(f"YOLO prediction result: {bounding_boxes}")
+        return result_image_path, bounding_boxes
     except Exception as e:
-        return f"Error predicting with YOLO: {str(e)}"
+        return f"Error predicting with YOLO: {str(e)}", []
+
+# The rest of your code remains unchanged
 
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
-        
-        file = request.files['file']
-        # Save the file to your desired location
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        
-        session['file_path'] = file.filename
-        
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(url_for('home'))
-
-        file = request.files['file']
-
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(url_for('home'))
-
-        if file and allowed_file(file.filename):
-            # Save the uploaded file to the 'uploads' folder
-            upload_folder = app.config['UPLOAD_FOLDER']
-            if not os.path.exists(upload_folder):
-                os.makedirs(upload_folder)
-
-            file_path = os.path.join(upload_folder, file.filename)
-            file.save(file_path)
-
-            return render_template('model_choice.html', filename=file.filename)
-        else:
-            flash('Invalid file type. Allowed types are png, jpg, jpeg, gif.')
-            return redirect(url_for('home'))
-
-    else:
-        return render_template('home.html')
-
-@app.route('/model_choice', methods=['POST', 'GET'])
-def model_choice():
-    try:
-
-
-        if file.filename == '':
-            flash('No selected file')
-            print('No selected file')
-            return redirect(url_for('home'))
-
-        if file and allowed_file(file.filename):
-            print(f"Model choice: {model_choice}")
-
-            # Construct the path to the uploaded file
-            upload_folder = app.config['UPLOAD_FOLDER']
-            file_path = os.path.join(upload_folder, file.filename)
-
-            if model_choice == 'vit':
-                result = predict_vit(file_path)
-                print(f"ViT prediction result: {result}")
-                return render_template('vit_result.html', result=result)
-
-            elif model_choice == 'yolo':
-                result = predict_yolo(file_path)
-                print(f"YOLO prediction result: {result}")
-                return render_template('yolo_result.html', result=result)
-            else:
-                flash('Invalid model choice')
-                print('Invalid model choice')
-                return redirect(url_for('home'))
-
-        else:
-            flash('Invalid file type. Allowed types are png, jpg, jpeg, gif.')
-            print('Invalid file type. Allowed types are png, jpg, jpeg, gif.')
-            return redirect(url_for('home'))
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-        flash('An error occurred. Please try again.')
-        return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
